1bba69c7dc1d5de3809c6c5afe9eae3e2b7fd277
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-01-29 19:58:15 +0900

    投稿・編集・削除昨日の実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,33 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
   end
+
+  def create
+    Review.create(review_params)
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    review = Review.find(params[:id])
+    review.update(review_params)
+    redirect_to root_path
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:title, :text, :image).merge(user_id: current_user.id, tag_id: 1)
+  end
 end
