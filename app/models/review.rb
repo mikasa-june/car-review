ce37543ceb9a33d01bafba8e73727be680575286
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-02-03 11:25:52 +0900

    マイページ完成（fav未実装）

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user
   has_many :comments
+  validates :title, presence: true
+  validates :image, presence: true
+  validates :text, presence: true
   
   def self.search(search)
     if search
