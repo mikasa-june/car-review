8fc319761b9bc41a60ef5e8add2e41992904eec9
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-02-11 13:21:24 +0900

    修正#1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = users.find(params[:id])
     @reviews = @user.reviews.order("updated_at DESC")
 
-    gets = LikeReview.where(user_id: [@user.id])
+    gets = LikeReview.where(user_id: [@user.id]).order("updated_at DESC").limit(40)
 
     @num = []
     gets.each do |get|
