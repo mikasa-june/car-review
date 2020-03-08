2ff0a5479ef7528bdb9a895a73bfb384c915b429
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-03-08 16:49:22 +0900

    tag_idを削除

--- db/migrate/20200129070947_create_reviews.rb
+++ db/migrate/20200129070947_create_reviews.rb
@@ -5,7 +5,6 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.text :text, null: false
       t.text :image
       t.integer :user_id, null: false, foreign_key: true
-      t.integer :tag_id, null: false, foreign_key: true
       t.timestamps
     end
   end
