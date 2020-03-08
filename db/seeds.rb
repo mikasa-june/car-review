f886f3836de4424ceae9493d78cb54fd35778ecb
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-03-08 10:46:43 +0900

    seed値を挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,244 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(
+  [
+    {
+      name: '日本',
+      group_id: 0,
+    },
+    {
+      name: 'ドイツ',
+      group_id: 0,
+    },
+    {
+      name: 'アメリカ',
+      group_id: 0,
+    },
+    {
+      name: 'イタリア',
+      group_id: 0,
+    },
+    {
+      name: 'フランス',
+      group_id: 0,
+    },
+    {
+      name: 'イギリス',
+      group_id: 0,
+    },
+    {
+      name: 'その他の国',
+      group_id: 0,
+    },
+    {
+      name: 'セダン',
+      group_id: 2,
+    },
+    {
+      name: 'クーペ',
+      group_id: 2,
+    },
+    {
+      name: 'ミニバン',
+      group_id: 2,
+    },
+    {
+      name: 'ワンボックス',
+      group_id: 2,
+    },
+    {
+      name: 'ステーションワゴン',
+      group_id: 2,
+    },
+    {
+      name: 'SUV',
+      group_id: 2,
+    },
+    {
+      name: 'コンパクトカー',
+      group_id: 2,
+    },
+    {
+      name: '軽',
+      group_id: 2,
+    },
+    {
+      name: 'レースカー',
+      group_id: 2,
+    },
+    {
+      name: 'その他のタイプ',
+      group_id: 2,
+    },
+    {
+      name: '高級車',
+      group_id: 3,
+    },
+    {
+      name: '特殊車両',
+      group_id: 3,
+    },
+    {
+      name: 'F1',
+      group_id: 3,
+    },
+    {
+      name: 'SUPER GT',
+      group_id: 3,
+    },
+    {
+      name: 'GT3',
+      group_id: 3,
+    },
+    {
+      name: 'NASCAR',
+      group_id: 3,
+    },
+    {
+      name: 'その他',
+      group_id: 3,
+    },
+    {
+      name: 'トヨタ',
+      group_id: 11,
+    },
+    {
+      name: 'ホンダ',
+      group_id: 11,
+    },
+    {
+      name: '日産',
+      group_id: 11,
+    },
+    {
+      name: 'マツダ',
+      group_id: 11,
+    },
+    {
+      name: 'SUZUKI',
+      group_id: 11,
+    },
+    {
+      name: 'ダイハツ',
+      group_id: 11,
+    },
+    {
+      name: 'SUBARU',
+      group_id: 11,
+    },
+    {
+      name: '',
+      group_id: ,
+    },
+    {
+      name: '三菱',
+      group_id: 11,
+    },
+    {
+      name: 'いすゞ',
+      group_id: 11,
+    },
+    {
+      name: 'その他日本メーカー',
+      group_id: 11,
+    },
+    {
+      name: 'ダイムラー・ベンツ',
+      group_id: 12,
+    },
+    {
+      name: 'BMW',
+      group_id: 12,
+    },
+    {
+      name: 'アウディ',
+      group_id: 12,
+    },
+    {
+      name: 'フェラーリ',
+      group_id: 12,
+    },
+    {
+      name: 'フィアット',
+      group_id: 12,
+    },
+    {
+      name: 'ルノー',
+      group_id: 12,
+    },
+    {
+      name: 'フォルクスワーゲン',
+      group_id: 12,
+    },
+    {
+      name: 'オペル',
+      group_id: 12,
+    },
+    {
+      name: 'プジョー',
+      group_id: 12,
+    },
+    {
+      name: 'シトロエン',
+      group_id: 12,
+    },
+    {
+      name: 'その他欧州メーカー',
+      group_id: 12,
+    },
+    {
+      name: 'キャデラック',
+      group_id: 13,
+    },
+    {
+      name: 'フォード',
+      group_id: 13,
+    },
+    {
+      name: 'クライスラー',
+      group_id: 13,
+    },
+    {
+      name: 'シボレー',
+      group_id: 13,
+    },
+    {
+      name: 'ビュイック',
+      group_id: 13,
+    },
+    {
+      name: 'ハマー',
+      group_id: 13,
+    },
+    {
+      name: 'ダッジ',
+      group_id: 13,
+    },
+    {
+      name: 'ジープ',
+      group_id: 13,
+    },
+    {
+      name: 'テスラ',
+      group_id: 13,
+    },
+    {
+      name: 'その他北米メーカー',
+      group_id: 13,
+    },
+    {
+      name: '上海汽車',
+      group_id: 14,
+    },
+    {
+      name: '現代自動車',
+      group_id: 14,
+    },
+    {
+      name: 'タタ・モーターズ',
+      group_id: 14,
+    },
+    {
+      name: 'その他メーカー',
+      group_id: 14,
+    }
+  ]
+)
